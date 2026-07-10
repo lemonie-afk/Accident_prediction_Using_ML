e2778e5c6e9ef01ff45e7aa3aafac1dd22f7c01e
Padmavathi <padmavathikaligotla158@gmail.com>
2026-07-10 10:59:34 +0530

    Update support_vector_machine.py

--- models/support_vector_machine.py
+++ models/support_vector_machine.py
@@ -1,1 +1,11 @@
+from sklearn.svm import SVC
 
+
+class SVMModel:
+
+    def train(self, X_train, y_train):
+        self.model = SVC()
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
