f96cda5beeefc0f8145b30c10fc2ba542ca2ca9b
Padmavathi <padmavathikaligotla158@gmail.com>
2026-07-10 11:00:41 +0530

    Update random_forest_model.py

--- models/random_forest_model.py
+++ models/random_forest_model.py
@@ -1,1 +1,11 @@
+from sklearn.ensemble import RandomForestClassifier
 
+
+class RandomForestModel:
+
+    def train(self, X_train, y_train):
+        self.model = RandomForestClassifier()
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
